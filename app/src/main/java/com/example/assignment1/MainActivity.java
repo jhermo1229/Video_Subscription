c7880a4350e6c27d24885a0ea06b46ca5c78b215
Jeffrey Hermo <Jhermo2967@conestogac.on.ca>
2022-02-16 10:48:28 -0500

    Added more fields, datepicker, autocompletetextview, spinner

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -5,29 +5,98 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.AutoCompleteTextView;
+import android.widget.CheckBox;
+import android.widget.DatePicker;
+import android.widget.EditText;
+import android.widget.RadioButton;
 import android.widget.Spinner;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText name;
+    RadioButton plan;
+    CheckBox addition;
+    Spinner noOfScreen;
+    AutoCompleteTextView province;
+    DatePicker videoDate;
+
+    private static final String[] PROVINCE = new String[] {
+            "Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador",
+            "Northwest Territories", "Nova Scotia", "Nunavut", "Ontario", "Prince Edward Island",
+            "Quebec", "Saskatchewan", "Yukon",
+    };
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        screenSpinner();
+        standardSpinner();
 
+        ArrayAdapter<String> adapter = new ArrayAdapter<String>(this,
+                android.R.layout.simple_dropdown_item_1line, PROVINCE);
+        AutoCompleteTextView textView = (AutoCompleteTextView)
+                findViewById(R.id.province);
+        textView.setAdapter(adapter);
+    }
+
+
+
+
+
+    public void onRadioButtonClicked(View view) {
+
+        // Is the button now checked?
+        boolean checked = ((RadioButton) view).isChecked();
+
+        // Check which radio button was clicked
+        switch(view.getId()) {
+            case R.id.radio_pirates:
+                if (checked)
+                    standardSpinner();
+                    break;
+            case R.id.radio_ninjas:
+                if (checked)
+                    premiumSpinner();
+                    break;
+        }
+    }
+
+    public void onCheckboxClicked(View view) {
+    }
+
+    private void screenSpinner() {
         Spinner spinner = (Spinner) findViewById(R.id.screen_spinner);
         // Create an ArrayAdapter using the string array and a default spinner layout
         ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
                 R.array.screen_array, android.R.layout.simple_spinner_item);
-// Specify the layout to use when the list of choices appears
+        // Specify the layout to use when the list of choices appears
         adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
-// Apply the adapter to the spinner
+        // Apply the adapter to the spinner
         spinner.setAdapter(adapter);
     }
 
-    public void onRadioButtonClicked(View view) {
+    private void standardSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.feature_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.standard_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
-    public void onCheckboxClicked(View view) {
+    private void premiumSpinner() {
+        Spinner spinner = (Spinner) findViewById(R.id.feature_spinner);
+        // Create an ArrayAdapter using the string array and a default spinner layout
+        ArrayAdapter<CharSequence> adapter = ArrayAdapter.createFromResource(this,
+                R.array.premium_array, android.R.layout.simple_spinner_item);
+        // Specify the layout to use when the list of choices appears
+        adapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        // Apply the adapter to the spinner
+        spinner.setAdapter(adapter);
     }
 
 
