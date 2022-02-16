971a727b267281bb830c7e6e3894e76d2a483ca5
Jeffrey Hermo <Jhermo2967@conestogac.on.ca>
2022-02-16 17:05:19 -0500

    Finished project but need to be refactored and commented

--- app/src/main/java/com/example/assignment1/MainActivity.java
+++ app/src/main/java/com/example/assignment1/MainActivity.java
@@ -2,25 +2,34 @@ package com.example.assignment1;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.ArrayAdapter;
 import android.widget.AutoCompleteTextView;
+import android.widget.Button;
 import android.widget.CheckBox;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.RadioButton;
 import android.widget.Spinner;
+import android.widget.Toast;
+
+import java.text.DecimalFormat;
 
 public class MainActivity extends AppCompatActivity {
 
     EditText name;
-    RadioButton plan;
-    CheckBox addition;
+    CheckBox addition1;
+    CheckBox addition2;
     Spinner noOfScreen;
+    Spinner feature;
     AutoCompleteTextView province;
     DatePicker videoDate;
-
+    Button submitButton;
+    String radioBut;
+    String choice;
+    private static final DecimalFormat df = new DecimalFormat("0.00");
     private static final String[] PROVINCE = new String[] {
             "Alberta", "British Columbia", "Manitoba", "New Brunswick", "Newfoundland and Labrador",
             "Northwest Territories", "Nova Scotia", "Nunavut", "Ontario", "Prince Edward Island",
@@ -39,11 +48,93 @@ public class MainActivity extends AppCompatActivity {
         AutoCompleteTextView textView = (AutoCompleteTextView)
                 findViewById(R.id.province);
         textView.setAdapter(adapter);
+
+        name = findViewById(R.id.name);
+        submitButton = findViewById(R.id.submitButton);
+        videoDate = findViewById(R.id.datePicker2);
+        province = findViewById(R.id.province);
+        noOfScreen = findViewById(R.id.screen_spinner);
+        addition1 = findViewById(R.id.checkbox_meat);
+        addition2 = findViewById(R.id.checkbox_cheese);
+        radioBut = "Standard";
+        feature = findViewById(R.id.feature_spinner);
+
+
+        submitButton.setOnClickListener(new View.OnClickListener(){
+
+
+            @Override
+            public void onClick(View v) {
+                String namess = name.getText().toString();
+                String video = videoDate.getMonth() + 1 + "/" + videoDate.getDayOfMonth() + "/" + videoDate.getYear();
+                String prov = province.getText().toString();
+                String screen = noOfScreen.getSelectedItem().toString();
+                String featureChoice = feature.getSelectedItem().toString();
+
+                Double cost = getPlanValue() + getAddition() + getScreenValue(screen) +  getFeature(featureChoice);
+
+
+
+                cost += cost * .13;
+
+                Toast.makeText(getApplicationContext(),"On " + video + ", for " + namess + " from " + prov + ", plan " + radioBut +
+                        ", with " + choice + ", " +  screen + " Screens, and "  + featureChoice + ", cost: $" + df.format(cost),Toast.LENGTH_LONG).show();
+
+
+
+            }
+        });
     }
 
+    private Double getFeature(String featureChoice) {
+        Double cost = new Double("0.00f");
+        if(featureChoice.equals("Multi-device Downloads")){
+            cost += 3.99;
+        }else if(featureChoice.equals("Watch on Laptop")){
+            cost += 2.99;
+        }else if(featureChoice.equals("Ultra HD")){
+            cost += 4.99;
+        }else if(featureChoice.equals("Priority Support")){
+            cost += 6.99;
+        }
+        return cost;
+    }
 
+    private Double getScreenValue(String screen){
+        Double cost = new Double("0.00f");
+        if(screen.equals("2")){
+            cost += 2.99;
+        }else if(screen.equals(("4"))){
+            cost += 4.99;
+        }
 
+        return cost;
+    }
 
+    private Double getPlanValue() {
+        Double cost = new Double("0.00f");
+        if(this.radioBut.equals("Standard")){
+            cost += 14.99;
+        }else if(this.radioBut.equals("Premium")){
+            cost += 18.99;
+        }
+        return cost;
+    }
+
+    private Double getAddition() {
+        Double cost = new Double("0.00f");
+        if(addition1.isChecked() && !addition2.isChecked()){
+            choice = addition1.getText().toString();
+            cost += 5.25;
+        }else if(!addition1.isChecked() && addition2.isChecked()){
+            choice = addition2.getText().toString();
+            cost += 6.49;
+        }else if(addition1.isChecked() && addition2.isChecked()){
+            choice = addition1.getText().toString() + " and " + addition2.getText().toString();
+            cost += 5.25 + 6.49;
+        }
+        return cost;
+    }
 
     public void onRadioButtonClicked(View view) {
 
@@ -55,10 +146,12 @@ public class MainActivity extends AppCompatActivity {
             case R.id.radio_pirates:
                 if (checked)
                     standardSpinner();
+                radioBut = ((RadioButton) view).getText().toString();
                     break;
             case R.id.radio_ninjas:
                 if (checked)
                     premiumSpinner();
+                radioBut = ((RadioButton) view).getText().toString();
                     break;
         }
     }
